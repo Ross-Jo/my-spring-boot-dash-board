43052beb531ffdd45082a381df1668334b811799
조법룡 <jby0711823@naver.com>
2021-01-10 19:29:07 +0900

    correct typo

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
+ABSPATH=$(readlink -f "$0")
+ABSDIR=$(dirname "$ABSPATH")
+source "${ABSDIR}"/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=my-spring-boot-dash-board
@@ -20,11 +20,11 @@ echo "> JAR_NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+chmod +x "$JAR_NAME"
 
 echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.profiles.active="$IDLE_PROFILE" -Dspring.config.location=classpath:/application.yml,classpath:/application-"$IDLE_PROFILE".yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
